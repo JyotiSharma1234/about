360735d40a587f35b4147fe07d1e505d78ffaaba
JYOTI SHARMA <jyoti.sharma@promobitech.com>
2020-11-08 22:47:38 +0530

    Home Page

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import {
+    makeStyles,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        height: '100%',
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+    section: {
+        marginTop: 10,
+        padding: 10,
+        overflow: 'auto',
+        height: 700,
+    },
+
+}));
+
+const Home = (props) => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.content}>
+            Home page
+        </div>
+    );
+};
+
+export default Home;
