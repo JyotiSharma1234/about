4258239c43bb695dc3b6dab2c2a3047d2937dc6a
JYOTI SHARMA <jyoti.sharma@promobitech.com>
2021-05-09 12:49:44 +0530

    Updates

---
+++ static/js/4.e539c254.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpabout=this.webpackJsonpabout||[]).push([[4],{188:function(t,e,n){"use strict";n.r(e);var a=n(2),i=(n(0),n(172)),o=Object(i.a)((function(t){return{paper:{height:"100%",padding:t.spacing(2),textAlign:"center",color:t.palette.text.secondary},section:{marginTop:10,padding:10,overflow:"auto",height:700}}}));e.default=function(t){var e=o();return Object(a.jsx)("div",{className:e.content,children:"Jyoti Sharma"})}}}]);
+//# sourceMappingURL=4.e539c254.chunk.js.map
