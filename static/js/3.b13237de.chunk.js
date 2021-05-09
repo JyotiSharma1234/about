780080c6b9f22901a76127348f28897f9a29a360
JYOTI SHARMA <jyoti.sharma@promobitech.com>
2021-05-09 13:02:28 +0530

    Updates

--- static/js/3.b13237de.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpabout=this.webpackJsonpabout||[]).push([[3],{189:function(t,e,n){"use strict";n.r(e);var a=n(2),i=(n(0),n(172)),c=Object(i.a)((function(t){return{paper:{height:"100%",padding:t.spacing(2),textAlign:"center",color:t.palette.text.secondary},section:{marginTop:10,padding:10,overflow:"auto",height:700}}}));e.default=function(t){var e=c();return Object(a.jsx)("div",{className:e.content,children:"Certificate page"})}}}]);
-//# sourceMappingURL=3.b13237de.chunk.js.map
