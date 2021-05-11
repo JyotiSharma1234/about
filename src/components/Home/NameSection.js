e81b13af1ab1925541ffe2538f79a0b4b89c625a
JYOTI SHARMA <jyoti.sharma@promobitech.com>
2021-05-11 15:53:15 +0530

    Typing effect

--- src/components/Home/NameSection.js
+++ src/components/Home/NameSection.js
@@ -1,20 +1,47 @@
 import React, { useState } from "react";
 import {
     makeStyles,
-    Button,
+    Box,
 } from "@material-ui/core";
+import "../CustomCss/typing.css"
+import Typewriter from "typewriter-effect";
 
 const useStyles = makeStyles((theme) => ({
 
+    container: {
+        height: 500,
+        borderStyle: 'double',  
+        color: '#fff',
+        borderColor: 'green',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
 }));
 
 const NameSection = (props) => {
     const classes = useStyles();
 
     return (
-        <div>
-            Jyoti Sharma
-        </div>
+        <Box className={classes.container}>    
+            <div className="typing_effect">
+                <Typewriter
+                    options={{
+                        // strings: ["Hello!<br>I'm Jyoti Sharma, web developer from Pune"],
+                        // , India.<br>Primarly expertised on Ruby on Rails<br>Also like to work on AngularJS, ReactJS"],
+                        autoStart: true,
+                        loop:true
+                    }}
+                    onInit={(typewriter)=> {
+                        typewriter
+                            .typeString("Explore! Learn! Grow!  repeat...")
+                            .pauseFor(1000)
+                            .deleteAll()
+                            .start();
+                    }}
+                />
+            </div>
+        </Box>
     );
 };
 
