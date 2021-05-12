7b7eb539297640baf6fba9b3ce31e52ad681fc9a
JYOTI SHARMA <jyoti.sharma@promobitech.com>
2021-05-12 17:24:37 +0530

    Who AM I? Page

--- src/components/Home/NameSection.js
+++ src/components/Home/NameSection.js
@@ -7,40 +7,65 @@ import "../CustomCss/typing.css"
 import Typewriter from "typewriter-effect";
 
 const useStyles = makeStyles((theme) => ({
-
     container: {
         height: 500,
-        borderStyle: 'double',  
         color: '#fff',
-        borderColor: 'green',
         display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
+        flexDirection: 'column'
+    },
+
+    heading:{
+        textAlign: 'center',
+        paddingBottom: 30,
+        fontSize: 32,
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        marginBottom: 20,
+        paddingBottom: 20,
+        position: 'relative',
+        color: '#fff',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    p: {
+        fontFamily: '"Open Sans", sans-serif',
+        fontSize: 20,
+        marginBottom: 20,
     },
 }));
 
 const NameSection = (props) => {
     const classes = useStyles();
-
     return (
-        <Box className={classes.container}>    
-            <div className="typing_effect">
+
+        <Box className={classes.container}>
+            <Box className={classes.heading}>
+                Who am I?
+            </Box>
+            <Box className={classes.p}>
+                Hello👋, I'm Jyoti Sharma
+            </Box>
+            <Box>
+                I am a web developer from Pune, India.
+                I keep my work practical, relevant and creative.
+                Have a look at my past projects, and feel free to reach out with any questions.
+            </Box>
+            <Box className="typing_effect">
                 <Typewriter
                     options={{
-                        // strings: ["Hello!<br>I'm Jyoti Sharma, web developer from Pune"],
-                        // , India.<br>Primarly expertised on Ruby on Rails<br>Also like to work on AngularJS, ReactJS"],
+                        strings: ["Explore! Learn! Grow!  repeat..."],
                         autoStart: true,
                         loop:true
                     }}
-                    onInit={(typewriter)=> {
-                        typewriter
-                            .typeString("Explore! Learn! Grow!  repeat...")
-                            .pauseFor(1000)
-                            .deleteAll()
-                            .start();
-                    }}
-                />
-            </div>
+                    // onInit={(typewriter)=> {
+                    //     typewriter
+                    //         .typeString("Explore! Learn! Grow!  repeat...")
+                    //         .pauseFor(1000)
+                    //         .deleteAll()
+                    //         .start();
+                    //     }}
+                        />
+            </Box>
         </Box>
     );
 };
